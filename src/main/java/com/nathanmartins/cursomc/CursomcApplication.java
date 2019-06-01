e83b17754e3a314478ba40d832f7f1ffd8db6f94
Nathan Martins <nathan.carneiro23011@gmail.com>
2019-06-01 12:19:37 -0300

    Rest testado

--- src/main/java/com/nathanmartins/cursomc/CursomcApplication.java
+++ src/main/java/com/nathanmartins/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.nathanmartinscursomc;
+package com.nathanmartins.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
