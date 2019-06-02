8554642c520e983225c76a4327ad5d64ac981b99
Nathan Martins <nathan.carneiro23011@gmail.com>
2019-06-02 11:31:15 -0300

    Criando operação de instaciação

--- src/main/java/com/nathanmartins/cursomc/CursomcApplication.java
+++ src/main/java/com/nathanmartins/cursomc/CursomcApplication.java
@@ -1,13 +1,35 @@
 package com.nathanmartins.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.nathanmartins.cursomc.domain.Categoria;
+import com.nathanmartins.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRespository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria (null, "Informática");
+		Categoria cat2 = new Categoria (null, "Escritório");		
+		
+		categoriaRespository.saveAll(Arrays.asList(cat1, cat2));
+				
+	}
+	
+	
 
 }
